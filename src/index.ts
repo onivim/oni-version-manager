062c095c7e9e74da256e8fbaaf8ad45016b52349
Bryan Phelps <bryphe@outlook.com>
2018-05-29 12:04:01 -0700

    Remove commented code

--- src/index.ts
+++ src/index.ts
@@ -99,48 +99,4 @@ if (argv._[0] === "path") {
             })
             .pipe(file)
     })()
-
-    // const metadata = await request(`https://api.onivim.io/v1/downloads/meta/${buildName}`)
-    // metadata.
 }
-
-console.dir(argv)
-
-// const log = (message: string, callback?: () => void) => {
-//     if (!callback) {
-//         console.log(message)
-//     } else {
-//         console.log("START: " + message)
-//         callback()
-//         console.log("END: " + message)
-//     }
-// }
-
-// const downloadFilePath =
-//     "https://api.onivim.io/v1/downloads/builds?branch=master&file=Oni-0.3.5-win.zip"
-
-// const downloadFile = (url: string, destinationFilePath: string): Promise<void> => {
-//     return new Promise((res, rej) => {
-//         log("Starting download...")
-//         const file = fs.createWriteStream(destinationFilePath)
-//         const callback = async (response: any) => {
-//             if (response.statusCode === 302) {
-//                 const location = response.headers.Location || response.headers.location
-//                 log("Received 302 redirect to: " + destinationFilePath)
-//                 https.get(location, callback)
-//             } else {
-//                 log("...piping download to file...")
-//                 response.pipe(file)
-
-//                 file.on("finish", () => {
-//                     log("Download complete")
-//                     ;(file as any).close(res)
-//                 })
-//             }
-//         }
-
-//         https.get(url, callback)
-//     })
-// }
-
-// downloadFile(downloadFilePath, path.join(cacheDirectory, "derp.zip"))
